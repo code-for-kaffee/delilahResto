bdf083e9821386ae0d005320039a431634422133
Federico <fedegomezavalos@gmail.com>
2020-05-06 21:01:14 -0300

    producys

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,8 +1,8 @@
 const { sequelize } = require('../db/db');
 const { QueryTypes } = require('sequelize');
-const {authenticate} =require ('../middleware/authenticationMiddleware')
- 
-module.exports.addProducts= (req, res, next) => {
+const { authenticate } = require('../middleware/authenticationMiddleware')
+
+module.exports.addProducts = (req, res, next) => {
     let data = req;
     this.add = async () => {
         try {
@@ -13,7 +13,7 @@ module.exports.addProducts= (req, res, next) => {
             }
             return res.status(200).send({ code: 'OK', message: `${response.estado}` });;
         } catch (error) {
-            throw res.status(409).send({error})
+            throw res.status(409).send({ error })
         }
     };
     const addProducts = (product) => {
@@ -27,47 +27,48 @@ module.exports.addProducts= (req, res, next) => {
 
 }
 
-module.exports.getProductsList = async (req, res, next) =>{
-try{
-    const productsList = await sequelize.query('SELECT * FROM products', { type : QueryTypes.SELECT });
-    const response = productsList;
-    return res.status(200).send({response})
-}
-catch(e){
-    error
-}
+module.exports.getProductsList = async (req, res, next) => {
+    try {
+        const productsList = await sequelize.query('SELECT * FROM products', { type: QueryTypes.SELECT });
+        const response = productsList;
+        return res.status(200).send({ response })
+    }
+    catch (error) {
+        return res.status(403).send({ Error: "Product List doesnt exist" });
+
+    }
 }
 module.exports.getProduct = async (req, res) => {
     try {
         const productId = req.params.id;
-        const product = await sequelize.query(`SELECT * FROM products WHERE id = ${productId}`, {type: QueryTypes.SELECT });
+        const product = await sequelize.query(`SELECT * FROM products WHERE id = ${productId}`, { type: QueryTypes.SELECT });
         const response = product;
-        return res.status(200).send({response});
+        return res.status(200).send({ response });
     } catch (error) {
-        return res.status(403).send({Error:"Product id doesnt exist"});
-        
+        return res.status(403).send({ Error: "Product id doesnt exist" });
+
     }
 }
-module.exports.deleteProduct = async (req, res) =>{
+module.exports.deleteProduct = async (req, res) => {
     try {
-        const productId = req.params.id;        
-        await sequelize.query(`DELETE FROM products WHERE id = ${productId}`, {type: QueryTypes.DELETE});
+        const productId = req.params.id;
+        await sequelize.query(`DELETE FROM products WHERE id = ${productId}`, { type: QueryTypes.DELETE });
         return res.status(200).send(`product sucessfully deleted`)
     } catch (error) {
-        return res.status(403).send({Error:"Product id doesnt exist"});       
+        return res.status(403).send({ Error: "Product id doesnt exist" });
     }
 
 }
 
 module.exports.updateProduct = async (req, res) => {
-try {
-    const updateParams = req.body;
-    console.log(updateParams);
-    const productId = req.params.id; 
-    for(prop in updateParams)       
-    await sequelize.query(`UPDATE products SET ${prop} = '${updateParams[prop]}'  WHERE id = ${productId}`, {type: QueryTypes.UPDATE });
-    return res.status(200).send(`Product updated!`);
-} catch (error) {
-    error
-}
+    try {
+        const updateParams = req.body;
+        console.log(updateParams);
+        const productId = req.params.id;
+        for (prop in updateParams)
+            await sequelize.query(`UPDATE products SET ${prop} = '${updateParams[prop]}'  WHERE id = ${productId}`, { type: QueryTypes.UPDATE });
+        return res.status(200).send(`Product updated!`);
+    } catch (error) {
+        error
+    }
 }
