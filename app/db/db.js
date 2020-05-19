89090deebf28ee8b188077ee75ce084f7d37d30f
fedegomezavalos@gmail.com <fedegomezavalos@gmail.com>
2020-05-18 21:28:18 -0300

    minor changes

--- app/db/db.js
+++ app/db/db.js
@@ -1,15 +1,15 @@
 const Sequelize = require('sequelize');
 //const sequelize = new Sequelize('mysql://zmuIZMfIcW:B1t0VeEZnH@remotemysql.com:3306/zmuIZMfIcW'); -> Descomentar para utilizar la db online
 const host = 'localhost';
-const database_name = 'DelilahResto';
+const dbName = 'DelilahResto';
 const user = 'root';
 const password = '';
 const port = '3306';
-const sequelize = new Sequelize('mysql://' + user + ':' + password + '@' + host + ':' + port + '/' + database_name);
+const sequelize = new Sequelize(`mysql://${user}:${password}@${host}:${port}/${dbName}`);
 
 sequelize.authenticate()
   .then(() => {
-    console.log('Conexión éxitosa a la base de datos: ' + database_name)
+    console.log(`Conexión éxitosa a la base de datos ${dbName}`)
   })
   .catch(err => {
     console.log('No se pudo conectar a MySQL')
